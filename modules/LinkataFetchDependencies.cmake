583451be710366fe267fbc1c934591a0484362f0
bowling-allie <allie.smith.epic@gmail.com>
2020-04-26 20:26:22 -0500

    added 'GIT_TAG_DEFAULT' parameter to 'linkata_fetch_option' fetchable packages now have cache variables to change the 'GIT_TAG' param (LINKATA_FETCH_${lib_name}_GIT_TAG) pakages are now fetched with the param 'GIT_SHALLOW' to speed up configure time

--- modules/LinkataFetchDependencies.cmake
+++ modules/LinkataFetchDependencies.cmake
@@ -3,13 +3,23 @@ include(CMakeDependentOption)
 
 option(LINKATA_FETCH_ALL "fetch all dependencies using cmake's FetchContent" OFF)
 
-macro(linkata_fetch_option lib_name default)
+function(linkata_fetch_option lib_name default)
+    cmake_parse_arguments(
+            _LINKATA_FETCH_OPTION
+            ""
+            "GIT_TAG_DEFAULT"
+            ""
+            ${ARGN})
     string(TOUPPER "${lib_name}" lib_name_upper)
     cmake_dependent_option(LINKATA_FETCH_${lib_name} "fetch ${lib_name} using cmake's FetchContent" "${default}"
             "NOT LINKATA_FETCH_ALL" ON)
+    if ("${_LINKATA_FETCH_OPTION_GIT_TAG_DEFAULT}" STREQUAL "")
+        set(_LINKATA_FETCH_OPTION_GIT_TAG_DEFAULT "origin/master")
+    endif ()
+    set(LINKATA_FETCH_${lib_name}_GIT_TAG "${_LINKATA_FETCH_OPTION_GIT_TAG_DEFAULT}" CACHE STRING "git branch name, tag or commit hash")
     if (LINKATA_FETCH_${lib_name} OR LINKATA_FETCH_ALL)
         set(LINKATA_FIND_${lib_name} OFF CACHE INTERNAL "")
     else ()
         set(LINKATA_FIND_${lib_name} ON CACHE INTERNAL "")
     endif ()
-endmacro()
+endfunction()
