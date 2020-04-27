30f7a7d5cb3cc998753bd3bff965143cace8498e
bowling-allie <allie.smith.epic@gmail.com>
2020-04-26 20:46:39 -0500

    im dumb was using 'it_.base()' when 'it_.base() - 1' was intended smh renamed temp return variables now using `lock_guard` instead of 'unique_lock'

--- src/lk/ready_task_queue.cpp
+++ src/lk/ready_task_queue.cpp
@@ -15,15 +15,15 @@ void lk::ready_task_queue::pushTask(
 }
 
 std::optional<std::pair<std::string, lk::ready_task_queue::task_type>> lk::ready_task_queue::popReadyTask() {
-  std::unique_lock<std::mutex> lock_(m_tasks_mutex);
+  std::lock_guard<std::mutex> lock_{m_tasks_mutex};
   for (auto it_ = m_tasks.rbegin(); it_ != m_tasks.rend(); ++it_) {
     auto &[id_, task_, ready_, thread_id_] = *it_;
     if (!thread_id_.has_value() || (std::this_thread::get_id() == thread_id_)) {
       if (ready_()) {
-        auto task_ret_{std::move(task_)};
-        std::string ret_id_{std::move(id_)};
-        m_tasks.erase(it_.base());
-        return {{std::move(ret_id_), std::move(task_ret_)}};
+        std::string id_out_{std::move(id_)};
+        auto task_out_{std::move(task_)};
+        m_tasks.erase(it_.base() - 1);
+        return {{std::move(id_out_), std::move(task_out_)}};
       }
     }
   }
